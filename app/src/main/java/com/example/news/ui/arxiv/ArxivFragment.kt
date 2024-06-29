e70b82fabb5a304961f279e7802ff030c5422b82
Gabin221 <serrurot19@gmail.com>
2024-06-29 14:46:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/news/ui/arxiv/ArxivFragment.kt
@@ -0,0 +1,32 @@
+package com.example.news.ui.arxiv
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.news.databinding.FragmentArxivBinding
+
+class ArxivFragment : Fragment() {
+
+    private var _binding: FragmentArxivBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentArxivBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
